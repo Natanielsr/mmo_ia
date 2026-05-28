06b070f1fbfa26d376ec2f8281906d8974b27abb
Natanielsr <contatonatanielsoares@gmail.com>
2026-05-28 11:59:46 -0300

    fix: ajustar tamanho de item para 40px e atualizar ícone do plate-armor

--- frontend/mmo-frontend/src/config/constants.ts
+++ frontend/mmo-frontend/src/config/constants.ts
@@ -1,4 +1,4 @@
 export const GRID_SIZE = 64;
 export const PLAYER_POSITION_OFFSET_X = 0;
 export const PLAYER_POSITION_OFFSET_Y = 7;
-export const WORLD_ITEM_SIZE = 32;
+export const WORLD_ITEM_SIZE = 32*1.25;
